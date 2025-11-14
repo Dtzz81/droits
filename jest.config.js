6fdcea2a639b1462a8a251743000a0332cf6abe4
denise.turkova <denise.turkova@madetech.com>
2025-11-14 12:56:58 +0000

    chore: skeleton of the passport feature test set

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
     testMatch: ["**/migration_tests/**/*.test.[jt]s?(x)"],
+    modulePathIgnorePatterns: ["/backoffice/src/bin/"],
 };
---
+++ migration_tests/passport-feature.test.js
@@ -0,0 +1,12 @@
+
+// not yet
+describe("User Authentication Migration", () => {
+    // This test  replicates a successful Passport login for a  user.
+    test("should allow a registered user to log in and access a protected resource", () => {
+    });
+
+    //more tests here  like:
+    // * Failed login with bad password
+    // * User registration flow
+    // * Session expiration
+});
